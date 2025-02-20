6b0cd1ed81a626365b1579bd78cc503c1bd5d6f2
Vaguetti <jvfox.vaguetti@gmail.com>
2025-02-20 14:50:08 -0300

    Adicionamos o histórico de rolls

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 // Espera o documento estar pronto antes de executar o script
-document.addEventListener("DOMContentLoaded", function () {
-  const rollButton = document.getElementById("roll_button");
-  const rollNumber = document.getElementById("roll_number");
-  const diceType = document.getElementById("dice_type");
+const rollButton = document.getElementById("roll_button");
+const rollNumber = document.getElementById("roll_number");
+const diceType = document.getElementById("dice_type");
+const historyRoll = document.getElementById("history_roll");
 
-  function rollDice() {
-    // Pega o valor selecionado no select
-    const maxNumber = parseInt(diceType.value);
-    const randomNumber = Math.floor(Math.random() * maxNumber) + 1;
-    rollNumber.textContent = randomNumber;
+function rollDice() {
+  // Pega o valor selecionado no select
+  const maxNumber = parseInt(diceType.value);
+  const randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+  rollNumber.textContent = randomNumber;
+
+  const rollEntry = document.createElement("h3");
+  rollEntry.textContent = `D${maxNumber}: ${randomNumber}`;
+
+  historyRoll.appendChild(rollEntry);
+
+  while (historyRoll.children.length > 5) {
+    historyRoll.removeChild(historyRoll.firstChild); // Remove sempre o primeiro <h3>
   }
+}
 
-  rollButton.addEventListener("click", rollDice);
-});
-fdfdffdf
+rollButton.addEventListener("click", rollDice);
